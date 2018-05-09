83d8c51d4f1bf4c31babe5015dbe25404d27cde7
Josh Walton <josh.walton@pearson.com>
2018-05-08 18:13:21 -0600

    fix bug with search and single command no vars options - useful for neptune

--- scripts/graphConf.js
+++ scripts/graphConf.js
@@ -1,9 +1,9 @@
 
 // configuration for the graph database access
-
+const host = "dev-ppact.cnyckbjgsrik.us-east-1-beta.rds.amazonaws.com"
 // For implementations like Neptune where only single commands are allowed per request
 // set to true
-const SINGLE_COMMANDS_AND_NO_VARS = false;
+const SINGLE_COMMANDS_AND_NO_VARS = true;
 
 // Time out for the REST protocol. Increase it if the graphDB is slow.
 const REST_TIMEOUT = 2000 
